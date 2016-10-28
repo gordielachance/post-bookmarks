c5871fdbf75b766e745730343c7e3f341f7c4074
rickybiscus <gordie.lachance@gmail.com>
2016-10-28 02:40:25 +0200

    minor

--- config.rb
+++ config.rb
@@ -4,6 +4,6 @@ sass_dir = "_inc/scss" # by Compass.app
 images_dir = "images" # by Compass.app 
 output_style = :expanded # by Compass.app 
 relative_assets = false # by Compass.app 
-line_comments = true # by Compass.app 
+line_comments = false # by Compass.app 
 sass_options = {:debug_info=>false} # by Compass.app 
 sourcemap = false # by Compass.app 
