2da44f6ae01650df6adb541adbf186bdf4553d6c
rickybiscus <gordie.lachance@gmail.com>
2016-11-26 02:08:59 +0100

    WIP : js function 'validate_url'

--- _inc/js/cp_links_admin.js
+++ _inc/js/cp_links_admin.js
@@ -4,48 +4,51 @@ jQuery(function($){
 
         // Look for changes in the value
         $('.cp_links_new .column-url input').live("change paste", function(event){
-            
-            if( !$(this).val() ) return;
-            
-            var cell_url = $(this).parents('td');
-            var row = cell_url.parents('.cp_links_new');
-            var cell_name = row.find('.column-name');
-            var name_input = cell_name.find('input');
-            
-            var name = cell_name.find('input').val();
-            if (name.length) return;
-            
-            var ajax_data = {
-                'action': 'cp_links_get_url_title',
-                'url': $(this).val()
-            };
-            $.ajax({
 
-                type: "post",
-                url: ajaxurl,
-                data:ajax_data,
-                dataType: 'json',
-                beforeSend: function() {
-                    cell_url.addClass('loading');
-                    cell_name.addClass('loading');
-                },
-                success: function(data){
-                    if (data.success === false) {
-                        cell_url.addClass('berror');
-                        console.log(data);
-                    }else{
-                        name_input.val(data.name);
+            if ( validate_url( $(this).val() ) ){
+
+                var cell_url = $(this).parents('td');
+                var row = cell_url.parents('.cp_links_new');
+                var cell_name = row.find('.column-name');
+                var name_input = cell_name.find('input');
+
+                var name = cell_name.find('input').val();
+                if (name.length) return;
+
+                var ajax_data = {
+                    'action': 'cp_links_get_url_title',
+                    'url': $(this).val()
+                };
+                $.ajax({
+
+                    type: "post",
+                    url: ajaxurl,
+                    data:ajax_data,
+                    dataType: 'json',
+                    beforeSend: function() {
+                        cell_url.addClass('loading');
+                        cell_name.addClass('loading');
+                    },
+                    success: function(data){
+                        if (data.success === false) {
+                            cell_url.addClass('berror');
+                            console.log(data);
+                        }else{
+                            name_input.val(data.name);
+                        }
+                    },
+                    error: function (xhr, ajaxOptions, thrownError) {
+                        console.log(xhr.status);
+                        console.log(thrownError);
+                    },
+                    complete: function() {
+                        cell_url.removeClass('loading');
+                        cell_name.removeClass('loading');
                     }
-                },
-                error: function (xhr, ajaxOptions, thrownError) {
-                    console.log(xhr.status);
-                    console.log(thrownError);
-                },
-                complete: function() {
-                    cell_url.removeClass('loading');
-                    cell_name.removeClass('loading');
-                }
-            })
+                })
+                
+            }
+
         });
 
         var add_new_section =  $('#add-link-section');
@@ -100,4 +103,27 @@ jQuery(function($){
         });
     })
 })
+
+//TO FIX TO IMPROVE
+function validate_url(url) {
+
+    if ( !url.trim() ) return false;
+    
+    //TO FIX
+
+    /*
+    //trick to get URL informations - http://stackoverflow.com/a/6944772/782013
+    var link = document.createElement('a');
+    link.href = url;
+
+    if ( !link.hostname ){
+        alert("no host");
+        return false;
+    }else{
+        alert("host : " + link.hostname);
+    }
+    */
+
+    return true;
+}
 
